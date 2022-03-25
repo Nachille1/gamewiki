5864b66bd16a70d0fb63920ff754d9bdaa6a1c8b
buffer <knefyodov@bk.ru>
2022-03-25 18:36:18 +0300

    Second Commit. Работа над формами профиля, регистрации и тд.

--- main.py
+++ main.py
@@ -1,12 +1,72 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from data import db_session
+from data.users import User
+from data.loginform import LoginForm
+from flask_login import LoginManager, login_user
+from forms.user import RegisterForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 @app.route('/')
 def main():
     return render_template('news.html')
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+@app.route('/user/<int:id>', methods=['GET', 'POST'])
+def profile(id):
+    return render_template('profile.html', title='Регистрация')
+
+
 if __name__ == '__main__':
+    db_session.global_init("db/GameWiki.db")
     app.run()
+
+#TODO: Доделать форму profile.html для профиля пользователя
