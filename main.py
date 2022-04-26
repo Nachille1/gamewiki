cddcb5da6390efb2839feeff28e1faaced6bc730
buffer <knefyodov@bk.ru>
2022-04-26 19:35:19 +0300

    try to load on heroku

--- main.py
+++ main.py
@@ -340,4 +340,5 @@ def add_news_random_game():
 # Запуск программы
 if __name__ == '__main__':
     db_session.global_init("db/GameWiki.db")
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
